c7a906e76486458125c73698a10dfcc11aeecad2
Emanuele Rocco Petrone <emanueleptrn97@gmail.com>
2019-01-13 16:26:10 +0100

    add example

---
+++ example/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/emaele/domainsdb"
+)
+
+func main() {
+	a, err := domainsdb.SearchForDomain("shitposting.io")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Printf("%#v", a.Domains[0])
+}
